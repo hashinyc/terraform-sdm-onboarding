3e9d303f3f4a128c7c9d07f2a73b8d7316140017
hashinyc <94708005+hashinyc@users.noreply.github.com>
2024-05-10 13:03:33 -0400

    Update onboarding.tf

--- onboarding.tf
+++ onboarding.tf
@@ -5,22 +5,22 @@ module "strongdm_onboarding" {
   prefix = "terraform-sdm"
 
   # EKS resources take approximately 20 min
-  # create_eks               = false
+  create_eks               = true
   # Mysql resources take approximately 15 min
-  # create_mysql             = true
+  create_mysql             = true
   # RDP resources take approximately 10 min
-  # create_rdp               = true
+  create_rdp               = true
   # HTTP resources take approximately 5 min
   # NOTE: Before creating HTTP resources, set up TLS here https://app.strongdm.com/app/datasources/websites
-  # create_http              = true
+  create_http              = true
   # SSH resources take approximately 5 min (requires create_http)
-  # create_ssh              = true
+  create_ssh              = true
   # Gateways take approximately 5 min
-  # create_strongdm_gateways = true
+  create_strongdm_gateways = true
 
   # VPC creation takes approximately 5 min
   # If set to false the default VPC will be used instead
-  # create_vpc = true
+  create_vpc = true
 
   # Tags will be added to strongDM and AWS resources.
   # tags = {}
